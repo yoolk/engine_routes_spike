2745c5f7e9e6e8ca7168446ae2b52a3711f15875
darren_jensen <darren.jensen@gmail.com>
2013-08-21 17:07:47 +0700

    updated readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 EngineRoutes::Application.routes.draw do
-	mount InstantWebsite::Engine, at: "/", constraints: InstantWebsite::DomainConstraint.new
+	# Uncomment the next line to test the InstantWebsite Engine routes
+	# mount InstantWebsite::Engine, at: "/", constraints: InstantWebsite::DomainConstraint.new
 	
   root :to => "home#index"
 
